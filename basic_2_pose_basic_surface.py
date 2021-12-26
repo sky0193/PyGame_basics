08bbbe759150c7af4e6cb6aa49a64565c997a167
sky0193 <olena.bykova93@gmail.com>
2021-12-26 16:59:43 +0100

    chg: enable flake 8

--- basic_2_pose_basic_surface.py
+++ basic_2_pose_basic_surface.py
@@ -15,7 +15,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-BG_COLLOR = (255,255,255)
+BG_COLLOR = (255, 255, 255)
 
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -28,20 +28,20 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
-            
+
         window.fill(BG_COLLOR)
-        
+
         surf_len = 50
         surf_width = 50
         surface = pygame.Surface((surf_len, surf_width))
-        
-        surf_color = (0,0,0)
+
+        surf_color = (0, 0, 0)
         surface.fill(surf_color)
-        
-        #rect = surface.get_rect()
+
+        rect = surface.get_rect()
         surf_center = (
-            (SCREEN_WIDTH - surface.get_width())/2,
-            (SCREEN_HEIGHT - surface.get_height())/2
+            (SCREEN_WIDTH - surface.get_width()) / 2,
+            (SCREEN_HEIGHT - surface.get_height()) / 2
         )
-        window.blit(surface,surf_center)
+        window.blit(surface, surf_center)
         pygame.display.flip()
--- basic_3_player.py
+++ basic_3_player.py
@@ -11,24 +11,24 @@ from pygame.locals import (
     QUIT
 )
 
+
 class Player(pygame.sprite.Sprite):
-    player_size = (75,25)
-    player_color = (0,0,0)
-    
+    player_size = (75, 25)
+    player_color = (0, 0, 0)
+
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.Surface(self.player_size)
         self.surf.fill(self.player_color)
         self.rect = self.surf.get_rect()
 
-
-
 pygame.init()
+
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-BG_COLLOR = (255,255,255)
+BG_COLLOR = (255, 255, 255)
 
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -42,12 +42,13 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
-            
+
         window.fill(BG_COLLOR)
-        
+
         surf_center = (
-            (SCREEN_WIDTH)/2,
-            (SCREEN_HEIGHT)/2
+            (SCREEN_WIDTH) / 2,
+            (SCREEN_HEIGHT) / 2
         )
-        window.blit(player.surf,surf_center)
+
+        window.blit(player.surf, surf_center)
         pygame.display.flip()
